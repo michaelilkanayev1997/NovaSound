e4a6de4a2626a14ddef8c0d70adc39656f729579
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-21 16:58:10 +0200

    Added Schema Validator

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -1,15 +1,21 @@
 import { CreateUser } from "#/@types/user";
+import { validate } from "#/middleware/validator";
 import User from "#/models/user";
+import { CreateUserSchema } from "#/utils/validationSchema";
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/create", async (req: CreateUser, res) => {
-  const { email, password, name } = req.body;
+router.post(
+  "/create",
+  validate(CreateUserSchema),
+  async (req: CreateUser, res) => {
+    const { email, password, name } = req.body;
 
-  const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password });
 
-  res.json({ user });
-});
+    res.json({ user });
+  }
+);
 
 export default router;
