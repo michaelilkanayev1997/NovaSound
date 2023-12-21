233b35ad8cb5f250e02293b3095f93f35ebe1d0f
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-21 17:35:37 +0200

    Sending Email via nodemailer

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,12 +1,30 @@
 import { RequestHandler } from "express";
+import nodemailer from "nodemailer";
 
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
+import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ name, email, password });
+
+  // send verification email
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: MAILTRAP_USER,
+      pass: MAILTRAP_PASS,
+    },
+  });
+
+  transport.sendMail({
+    to: user.email,
+    from: "auth@myapp.com",
+    html: "<h1>123445</h1>",
+  });
 
   res.status(201).json({ user });
 };
--- server/src/utils/variables.ts
+++ server/src/utils/variables.ts
@@ -1,3 +1,3 @@
 const { env } = process as { env: { [key: string]: string } };
 
-export const MONGO_URI = env.MONGO_URI;
+export const { MONGO_URI, MAILTRAP_USER, MAILTRAP_PASS } = env;
