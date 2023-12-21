7d56274d74ca1fc0bcf79f8530b559d5316b5a80
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-21 15:00:43 +0200

    Added User Model

---
+++ server/src/models/user.ts
@@ -0,0 +1,66 @@
+// interface (typescript)
+
+import { Model, model, ObjectId, Schema } from "mongoose";
+
+interface UserDocument {
+  name: string;
+  email: string;
+  password: string;
+  verified: boolean;
+  avatar?: { url: String; publicId: string };
+  tokens: string[];
+  favorites: ObjectId[];
+  followers: ObjectId[];
+  followings: ObjectId[];
+}
+
+const userSchema = new Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: Object,
+      url: String,
+      publicId: String,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    favorites: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Audio",
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followings: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    tokens: [String],
+  },
+  { timestamps: true }
+);
+
+export default model("User", userSchema) as Model<UserDocument>;
