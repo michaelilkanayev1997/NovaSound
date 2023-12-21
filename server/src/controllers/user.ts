cf055d47f18bc59e5cd9036b45760fe9a22b36e0
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-21 22:31:43 +0200

    Sending Mail with Attachment

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,11 +1,13 @@
 import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
+import path from "path";
 
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
 import EmailVerificationToken from "#/models/emailVerificationToken";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 import { generateToken } from "#/utils/helper";
+import { generateTemplate } from "#/mail/template";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -28,10 +30,32 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     token,
   });
 
+  const welcomeMessage = `Hi ${name}, welcome to NovaSound! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: `<h1>Your verification token is ${token}</h1>`,
+    subject: "Welcome to NovaSound",
+    html: generateTemplate({
+      title: "Welcome to NovaSound",
+      message: welcomeMessage,
+      logo: "cid:logo",
+      banner: "cid:welcome",
+      link: "#",
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: "logo.png",
+        path: path.join(__dirname, "../mail/logo.png"),
+        cid: "logo",
+      },
+      {
+        filename: "welcome.png",
+        path: path.join(__dirname, "../mail/welcome.png"),
+        cid: "welcome",
+      },
+    ],
   });
 
   res.status(201).json({ user });
