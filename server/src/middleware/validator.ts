f9fb8e3675b52c77301f361b6476a03bfb193df0
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-21 17:20:44 +0200

    Code Refactor

--- server/src/middleware/validator.ts
+++ server/src/middleware/validator.ts
@@ -3,7 +3,8 @@ import * as yup from "yup";
 
 export const validate = (schema: any): RequestHandler => {
   return async (req, res, next) => {
-    if (!req.body) return res.json({ error: "Empty body is not allowed!" });
+    if (!req.body)
+      return res.status(422).json({ error: "Empty body is not allowed!" });
 
     const schemaToValidate = yup.object({ body: schema });
 
@@ -18,7 +19,7 @@ export const validate = (schema: any): RequestHandler => {
       next();
     } catch (error) {
       if (error instanceof yup.ValidationError) {
-        res.json({ error: error.message });
+        res.status(422).json({ error: error.message });
       }
     }
   };
