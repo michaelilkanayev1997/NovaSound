bf01374a156650b7c421250a6b0da6dfd800af5e
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-23 19:02:26 +0200

    Validating Token & ID

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 
 import { create, verifyEmail } from "#/controllers/user";
 import { validate } from "#/middleware/validator";
-import { CreateUserSchema } from "#/utils/validationSchema";
+import {
+  CreateUserSchema,
+  EmailVerificationBody,
+} from "#/utils/validationSchema";
 
 const router = Router();
 
 router.post("/create", validate(CreateUserSchema), create);
-router.post("/verify-email", verifyEmail);
+router.post("/verify-email", validate(EmailVerificationBody), verifyEmail);
 
 export default router;
