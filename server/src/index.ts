d4f781f7371b502f895cc58bfde9202def176286
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-26 11:14:26 +0200

    Creating a public route

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ const app = express();
 // register middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("src/public"));
 
 app.use("/auth", authRouter);
 
