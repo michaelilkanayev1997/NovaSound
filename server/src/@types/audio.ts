3b1651ed67cf353566101a1d75ddb0adcc7b6bc4
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-12-30 17:20:49 +0200

    Added Playlist Update Route

--- server/src/@types/audio.ts
+++ server/src/@types/audio.ts
@@ -7,3 +7,12 @@ export type PopulatedFavList = AudioDocument<{ _id: ObjectId; name: string }>;
 export interface CreatePlaylistRequest extends Request {
   body: { title: string; resId: string; visibility: "public" | "private" };
 }
+
+export interface UpdatePlaylistRequest extends Request {
+  body: {
+    title: string;
+    id: string;
+    item: string;
+    visibility: "public" | "private";
+  };
+}
