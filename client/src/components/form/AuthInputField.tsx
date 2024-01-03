8b2ed13d327bd9ba3e4b210cf3c89f81e773cfe7
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-03 23:30:49 +0200

    Added Shake Animation with Reanimated

--- client/src/components/form/AuthInputField.tsx
+++ client/src/components/form/AuthInputField.tsx
@@ -1,7 +1,7 @@
 import AppInput from '@ui/AppInput';
 import colors from '@utils/colors';
 import {useFormikContext} from 'formik';
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {
   View,
   StyleSheet,
@@ -10,6 +10,13 @@ import {
   StyleProp,
   ViewStyle,
 } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
 interface Props {
   name: string;
@@ -22,6 +29,8 @@ interface Props {
 }
 
 const AuthInputField: FC<Props> = props => {
+  const inputTransformValue = useSharedValue(0);
+
   const {handleChange, values, errors, handleBlur, touched} = useFormikContext<{
     [key: string]: string;
   }>();
@@ -38,8 +47,30 @@ const AuthInputField: FC<Props> = props => {
 
   const errorMsg = touched[name] && errors[name] ? errors[name] : '';
 
+  const shakeUI = () => {
+    inputTransformValue.value = withSequence(
+      withTiming(-6, {duration: 50}),
+      withSpring(0, {
+        damping: 8,
+        mass: 0.5,
+        stiffness: 1000,
+        restDisplacementThreshold: 0.1,
+      }),
+    );
+  };
+
+  const inputStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: inputTransformValue.value}],
+    };
+  });
+
+  useEffect(() => {
+    if (errorMsg) shakeUI();
+  }, [errorMsg]);
+
   return (
-    <View style={[styles.container, containerStyle]}>
+    <Animated.View style={[containerStyle, inputStyle]}>
       <View style={styles.labelContainer}>
         <Text style={styles.label}>{label}</Text>
         <Text style={styles.errorMsg}>{errorMsg}</Text>
@@ -53,12 +84,11 @@ const AuthInputField: FC<Props> = props => {
         value={values[name]}
         onBlur={handleBlur(name)}
       />
-    </View>
+    </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
   labelContainer: {
     flexDirection: 'row',
     alignItems: 'center',
