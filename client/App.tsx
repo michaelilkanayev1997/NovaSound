df57286cae7143226eb907c5572ed6c06790d93b
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-04 11:47:14 +0200

    Added Verification Page using OTP Field Ui

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,7 @@
 import LostPassword from '@views/auth/LostPassword';
 import SignIn from '@views/auth/SignIn';
 import SignUp from '@views/auth/SignUp';
+import Verification from '@views/auth/Verification';
 import {I18nManager} from 'react-native';
 
 // Force LTR text direction
@@ -8,7 +9,7 @@ I18nManager.allowRTL(false);
 I18nManager.forceRTL(false);
 
 const App = () => {
-  return <LostPassword />;
+  return <Verification />;
 };
 
 export default App;
