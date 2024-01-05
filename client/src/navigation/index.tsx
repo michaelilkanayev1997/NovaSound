e0b46b28e013427a644024ff10e2aeafba1a3a0c
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-06 00:01:08 +0200

    Fetching isAuth From Async Storage

--- client/src/navigation/index.tsx
+++ client/src/navigation/index.tsx
@@ -1,14 +1,38 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import AuthNavigator from './AuthNavigator';
-import {useSelector} from 'react-redux';
-import {getAuthState} from 'src/store/auth';
+import {useDispatch, useSelector} from 'react-redux';
+import {getAuthState, updateLoggedInState, updateProfile} from 'src/store/auth';
 import TabNavigator from './TabNavigator';
+import {Keys, getFromAsyncStorage} from '@utils/asyncStorage';
+import client from 'src/api/client';
 
 interface Props {}
 
 const AppNavigator: FC<Props> = props => {
   const {loggedIn} = useSelector(getAuthState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchAuthInfo = async () => {
+      try {
+        const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+        if (!token) return;
+
+        const {data} = await client.get('/auth/is-auth', {
+          headers: {Authorization: 'Bearer ' + token},
+        });
+
+        dispatch(updateProfile(data.profile));
+        dispatch(updateLoggedInState(true));
+      } catch (error) {
+        console.log('Auth error: ', error);
+      }
+    };
+
+    fetchAuthInfo();
+  }, []);
+
   return (
     <NavigationContainer>
       {loggedIn ? <TabNavigator /> : <AuthNavigator />}
