323efb8afa1a469898ad3ea09a327921895daaf2
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-07 20:20:04 +0200

    Refactor Hooks

---
+++ client/src/hooks/query.ts
@@ -0,0 +1,23 @@
+import {useQuery} from 'react-query';
+import {useDispatch} from 'react-redux';
+import {AudioData} from 'src/@types/audio';
+import catchAsyncError from 'src/api/catchError';
+import client from 'src/api/client';
+import {updateNotification} from 'src/store/notification';
+
+const fetchLatest = async (): Promise<AudioData[]> => {
+  const {data} = await client('/audio/latest');
+  return data.audios;
+};
+
+export const useFetchLatestAudios = () => {
+  const dispatch = useDispatch();
+
+  return useQuery(['latest-uploads'], {
+    queryFn: () => fetchLatest(),
+    onError(err) {
+      const errorMessage = catchAsyncError(err);
+      dispatch(updateNotification({message: errorMessage, type: 'error'}));
+    },
+  });
+};
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,44 +1,28 @@
 import {FC, useState} from 'react';
-import {View, StyleSheet, Text, Button} from 'react-native';
-import {useQuery} from 'react-query';
-import {useDispatch} from 'react-redux';
-import catchAsyncError from 'src/api/catchError';
-import client from 'src/api/client';
-import {updateNotification} from 'src/store/notification';
+import {View, StyleSheet, Text} from 'react-native';
+import {useFetchLatestAudios} from 'src/hooks/query';
 
 interface Props {}
 
-const fetchLatest = async () => {
-  const {data} = await client('/audio/latest');
-  return data.audios;
-};
-
 const Home: FC<Props> = props => {
-  const dispatch = useDispatch();
+  const {data, isLoading} = useFetchLatestAudios();
 
-  const query = useQuery(['latest-uploads'], {
-    queryFn: () => fetchLatest(),
-    onError(err) {
-      const errorMessage = catchAsyncError(err);
-      dispatch(updateNotification({message: errorMessage, type: 'error'}));
-    },
-  });
-
-  console.log(query);
+  if (isLoading)
+    return (
+      <View style={styles.container}>
+        <Text style={{color: 'white', fontSize: 25}}>Loading</Text>
+      </View>
+    );
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Button
-        title="lol"
-        onPress={() => {
-          dispatch(
-            updateNotification({
-              message: 'Uploaded successfully !',
-              type: 'info',
-            }),
-          );
-        }}></Button>
+      {data?.map(item => {
+        return (
+          <Text key={item.id} style={{color: 'white', paddingVertical: 10}}>
+            {item.title}
+          </Text>
+        );
+      })}
     </View>
   );
 };
