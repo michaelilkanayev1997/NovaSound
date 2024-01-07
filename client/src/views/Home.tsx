746b23c42dcf5c7265958ab4b16753fef9e650ac
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-07 18:16:23 +0200

    Using React Query for Latest-Uploads

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,12 +1,31 @@
 import {FC} from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
+import {useQuery} from 'react-query';
 import {useDispatch} from 'react-redux';
+import catchAsyncError from 'src/api/catchError';
+import client from 'src/api/client';
 import {updateNotification} from 'src/store/notification';
 
 interface Props {}
 
+const fetchLatest = async () => {
+  const {data} = await client('/audio/latest');
+  return data.audios;
+};
+
 const Home: FC<Props> = props => {
   const dispatch = useDispatch();
+
+  const query = useQuery(['latest-uploads'], {
+    queryFn: () => fetchLatest(),
+    onError(err) {
+      const errorMessage = catchAsyncError(err);
+      dispatch(updateNotification({message: errorMessage, type: 'error'}));
+    },
+  });
+
+  console.log(query);
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
