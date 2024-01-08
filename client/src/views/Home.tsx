05f6858573626b6a3f27468031ec8bf2f592fa78
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-08 02:54:24 +0200

    Adding To Favories (Client)

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -5,11 +5,55 @@ import {FC, useState} from 'react';
 import {View, StyleSheet, Pressable, Text} from 'react-native';
 import MaterialComIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import colors from '@utils/colors';
+import {AudioData} from 'src/@types/audio';
+import client from 'src/api/client';
+import {Keys, getFromAsyncStorage} from '@utils/asyncStorage';
+import {updateNotification} from 'src/store/notification';
+import catchAsyncError from 'src/api/catchError';
+import {useDispatch} from 'react-redux';
 
 interface Props {}
 
 const Home: FC<Props> = props => {
   const [showOptions, setShowOptions] = useState(false);
+  const [selectedAudio, setSelectedAudio] = useState<AudioData>();
+  const dispatch = useDispatch();
+
+  const handleOnFavPress = async () => {
+    if (!selectedAudio) return;
+    // send request with the audio id that we want to add fav
+
+    try {
+      const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+
+      const {data} = await client.post(
+        '/favorite?audioId=' + selectedAudio.id,
+        null,
+        {
+          headers: {Authorization: 'Bearer ' + token},
+        },
+      );
+      dispatch(
+        updateNotification({
+          message:
+            data.status === 'added'
+              ? 'Added To Favorites'
+              : 'Removed From Favorites',
+          type: 'success',
+        }),
+      );
+    } catch (error) {
+      const errorMessage = catchAsyncError(error);
+      dispatch(updateNotification({message: errorMessage, type: 'error'}));
+    }
+    setSelectedAudio(undefined);
+    setShowOptions(false);
+  };
+
+  const handleOnLongPress = (audio: AudioData) => {
+    setSelectedAudio(audio);
+    setShowOptions(true);
+  };
 
   return (
     <View style={styles.container}>
@@ -17,17 +61,13 @@ const Home: FC<Props> = props => {
         onAudioPress={item => {
           console.log(item);
         }}
-        onAudioLongPress={item => {
-          setShowOptions(true);
-        }}
+        onAudioLongPress={handleOnLongPress}
       />
       <RecommendedAudios
         onAudioPress={item => {
           console.log(item);
         }}
-        onAudioLongPress={item => {
-          setShowOptions(true);
-        }}
+        onAudioLongPress={handleOnLongPress}
       />
       <OptionsModal
         visible={showOptions}
@@ -35,12 +75,19 @@ const Home: FC<Props> = props => {
           setShowOptions(false);
         }}
         options={[
-          {title: 'Add to playlist', icon: 'playlist-music'},
-          {title: 'Add to favorites', icon: 'cards-heart'},
+          {
+            title: 'Add to playlist',
+            icon: 'playlist-music',
+          },
+          {
+            title: 'Add to favorites',
+            icon: 'cards-heart',
+            onPress: handleOnFavPress,
+          },
         ]}
         renderItem={item => {
           return (
-            <Pressable style={styles.optionContainer}>
+            <Pressable onPress={item.onPress} style={styles.optionContainer}>
               <MaterialComIcon
                 size={24}
                 color={colors.PRIMARY}
