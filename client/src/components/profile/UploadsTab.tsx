fec196574d6f597f2a778b5e718ce6b46f77bad0
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-08 22:42:00 +0200

    Refactor Audio List Item

--- client/src/components/profile/UploadsTab.tsx
+++ client/src/components/profile/UploadsTab.tsx
@@ -1,13 +1,7 @@
+import AudioListItem from '@ui/AudioListItem';
 import colors from '@utils/colors';
 import {FC} from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  Pressable,
-  Image,
-  ScrollView,
-} from 'react-native';
+import {View, StyleSheet, ScrollView} from 'react-native';
 import {useFetchUploadsByProfile} from 'src/hooks/query';
 
 interface Props {}
@@ -15,26 +9,10 @@ interface Props {}
 const UploadsTab: FC<Props> = props => {
   const {data, isLoading} = useFetchUploadsByProfile();
 
-  const getSource = (poster?: string) => {
-    return poster ? {uri: poster} : require('../../assets/no-poster.jpg');
-  };
-
   return (
     <ScrollView style={styles.container}>
       {data?.map(item => {
-        return (
-          <Pressable style={styles.listItem} key={item.id}>
-            <Image source={getSource(item.poster)} style={styles.poster} />
-            <View style={styles.titleContainer}>
-              <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
-                {item.title}
-              </Text>
-              <Text style={styles.owner} numberOfLines={1} ellipsizeMode="tail">
-                {item.owner.name}
-              </Text>
-            </View>
-          </Pressable>
-        );
+        return <AudioListItem audio={item} />;
       })}
     </ScrollView>
   );
@@ -42,28 +20,6 @@ const UploadsTab: FC<Props> = props => {
 
 const styles = StyleSheet.create({
   container: {},
-  listItem: {
-    flexDirection: 'row',
-    backgroundColor: colors.OVERLAY,
-    marginBottom: 15,
-    borderRadius: 5,
-  },
-  titleContainer: {
-    flex: 1,
-    padding: 5,
-  },
-  poster: {
-    width: 50,
-    height: 50,
-  },
-  title: {
-    color: colors.CONTRAST,
-    fontWeight: '700',
-  },
-  owner: {
-    color: colors.SECONDARY,
-    fontWeight: '700',
-  },
 });
 
 export default UploadsTab;
---
+++ client/src/ui/AudioListItem.tsx
@@ -0,0 +1,56 @@
+import colors from '@utils/colors';
+import {FC} from 'react';
+import {View, StyleSheet, Pressable, Image, Text} from 'react-native';
+import {AudioData} from 'src/@types/audio';
+
+interface Props {
+  audio: AudioData;
+  onPress?(): void;
+}
+
+const AudioListItem: FC<Props> = ({audio, onPress}) => {
+  const getSource = (poster?: string) => {
+    return poster ? {uri: poster} : require('../assets/no-poster.jpg');
+  };
+
+  return (
+    <Pressable onPress={onPress} style={styles.listItem}>
+      <Image source={getSource(audio.poster)} style={styles.poster} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
+          {audio.title}
+        </Text>
+        <Text style={styles.owner} numberOfLines={1} ellipsizeMode="tail">
+          {audio.owner.name}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    flexDirection: 'row',
+    backgroundColor: colors.OVERLAY,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  titleContainer: {
+    flex: 1,
+    padding: 5,
+  },
+  poster: {
+    width: 50,
+    height: 50,
+  },
+  title: {
+    color: colors.CONTRAST,
+    fontWeight: '700',
+  },
+  owner: {
+    color: colors.SECONDARY,
+    fontWeight: '700',
+  },
+});
+
+export default AudioListItem;
