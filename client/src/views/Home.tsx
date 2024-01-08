d3e3a26de68db0f46592af7944a90a12a04b2f1b
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-08 02:02:49 +0200

    Added Options Modal

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,11 +1,16 @@
 import LatestUploads from '@components/LatestUploads';
+import OptionsModal from '@components/OptionsModal';
 import RecommendedAudios from '@components/RecommendedAudios';
-import {FC} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {FC, useState} from 'react';
+import {View, StyleSheet, Pressable, Text} from 'react-native';
+import MaterialComIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '@utils/colors';
 
 interface Props {}
 
 const Home: FC<Props> = props => {
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
     <View style={styles.container}>
       <LatestUploads
@@ -13,7 +18,7 @@ const Home: FC<Props> = props => {
           console.log(item);
         }}
         onAudioLongPress={item => {
-          console.log('long press');
+          setShowOptions(true);
         }}
       />
       <RecommendedAudios
@@ -21,7 +26,29 @@ const Home: FC<Props> = props => {
           console.log(item);
         }}
         onAudioLongPress={item => {
-          console.log('long press');
+          setShowOptions(true);
+        }}
+      />
+      <OptionsModal
+        visible={showOptions}
+        onRequestClose={() => {
+          setShowOptions(false);
+        }}
+        options={[
+          {title: 'Add to playlist', icon: 'playlist-music'},
+          {title: 'Add to favorites', icon: 'cards-heart'},
+        ]}
+        renderItem={item => {
+          return (
+            <Pressable style={styles.optionContainer}>
+              <MaterialComIcon
+                size={24}
+                color={colors.PRIMARY}
+                name={item.icon}
+              />
+              <Text style={styles.optionLabel}>{item.title}</Text>
+            </Pressable>
+          );
         }}
       />
     </View>
@@ -32,6 +59,12 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
+  optionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  optionLabel: {color: colors.PRIMARY, fontSize: 16, marginLeft: 5},
 });
 
 export default Home;
