599e5dd8ad4f855d93605e13a928b6d36ac607d2
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-10 19:05:24 +0200

    Added Pausing Audio

--- client/src/hooks/useAudioController.ts
+++ client/src/hooks/useAudioController.ts
@@ -1,4 +1,8 @@
-import TrackPlayer, {Track} from 'react-native-track-player';
+import TrackPlayer, {
+  State,
+  Track,
+  usePlaybackState,
+} from 'react-native-track-player';
 import {AudioData} from 'src/@types/audio';
 
 const updateQueue = async (data: AudioData[]) => {
@@ -17,14 +21,25 @@ const updateQueue = async (data: AudioData[]) => {
 };
 
 const useAudioController = () => {
+  const {state: playbackState} = usePlaybackState() as {state?: State};
+
+  const isPlayerReady = playbackState !== State.None;
+
+  console.log(isPlayerReady);
+
   const onAudioPress = async (item: AudioData, data: AudioData[]) => {
-    await updateQueue(data);
+    if (!isPlayerReady) {
+      // Playing Audio for the first time.
+      await updateQueue(data);
+      const index = data.findIndex(audio => audio.id === item.id);
+      await TrackPlayer.skip(index);
+      await TrackPlayer.play();
+    }
 
-    const index = data.findIndex(audio => audio.id === item.id);
-
-    await TrackPlayer.skip(index);
-
-    await TrackPlayer.play();
+    if (playbackState === State.Playing) {
+      // Audio is already playing
+      await TrackPlayer.pause();
+    }
   };
 
   return {onAudioPress};
