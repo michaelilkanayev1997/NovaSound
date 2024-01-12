f0b458fce1509012a5f6cbaacebe1b483368ca3e
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-12 23:32:44 +0200

    Rendering Histories

--- client/src/components/profile/HistoryTab.tsx
+++ client/src/components/profile/HistoryTab.tsx
@@ -1,25 +1,73 @@
+import AudioListLoadingUI from '@ui/AudioListLoadingUI';
 import EmptyRecords from '@ui/EmptyRecords';
+import colors from '@utils/colors';
 import {FC} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, ScrollView, Pressable} from 'react-native';
 import {useFetchHistories} from 'src/hooks/query';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 interface Props {}
 
 const HistoryTab: FC<Props> = props => {
   const {data, isLoading} = useFetchHistories();
 
+  if (isLoading) return <AudioListLoadingUI />;
+
   if (!data || !data[0]?.audios.length)
     return <EmptyRecords title="There is no histories available!" />;
 
   return (
-    <View style={styles.container}>
-      <Text style={{fontSize: 20, color: 'white'}}>History</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {data.map((item, mainIndex) => {
+        return (
+          <View key={item.date + mainIndex}>
+            <Text style={styles.date}>{item.date}</Text>
+            <View style={styles.listContainer}>
+              {item.audios.map((audio, index) => {
+                return (
+                  <View key={audio.id + index} style={styles.history}>
+                    <Text style={styles.historyTitle}>{audio.title}</Text>
+                    <Pressable>
+                      <AntDesign
+                        name="close"
+                        color={colors.CONTRAST}
+                        size={14}
+                      />
+                    </Pressable>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {},
+  listContainer: {
+    marginTop: 10,
+    paddingLeft: 10,
+  },
+  date: {
+    color: colors.SECONDARY,
+  },
+  historyTitle: {
+    color: colors.CONTRAST,
+    paddingHorizontal: 5,
+    fontWeight: '700',
+    flex: 1,
+  },
+  history: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: colors.PRIMARY_DARK1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
 });
 
 export default HistoryTab;
