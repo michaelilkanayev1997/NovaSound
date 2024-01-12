ec72cf4dca27c5d89e92eed79228bd2ec4a0b432
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-12 18:06:44 +0200

    Added Audio List Loading & Play Animation

--- client/src/ui/AudioListModal.tsx
+++ client/src/ui/AudioListModal.tsx
@@ -4,27 +4,54 @@ import {FlatList, StyleSheet, Text, View} from 'react-native';
 import AppModal from './AppModal';
 import {AudioData} from 'src/@types/audio';
 import AudioListItem from './AudioListItem';
+import AudioListLoadingUI from './AudioListLoadingUI';
+import {useSelector} from 'react-redux';
+import {getPlayerState} from 'src/store/player';
 
 interface Props {
   data: AudioData[];
   header?: string;
   visible: boolean;
   onRequestClose(): void;
+  onItemPress(item: AudioData, data: AudioData[]): void;
+  loading?: boolean;
 }
 
-const AudioListModal: FC<Props> = ({header, data, visible, onRequestClose}) => {
+const AudioListModal: FC<Props> = ({
+  header,
+  loading,
+  data,
+  visible,
+
+  onItemPress,
+  onRequestClose,
+}) => {
+  const {onGoingAudio} = useSelector(getPlayerState);
+
   return (
     <AppModal visible={visible} onRequestClose={onRequestClose}>
       <View style={styles.container}>
-        <Text style={styles.header}>{header}</Text>
+        {loading ? (
+          <AudioListLoadingUI />
+        ) : (
+          <>
+            <Text style={styles.header}>{header}</Text>
 
-        <FlatList
-          data={data}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => {
-            return <AudioListItem audio={item} />;
-          }}
-        />
+            <FlatList
+              data={data}
+              keyExtractor={item => item.id}
+              renderItem={({item}) => {
+                return (
+                  <AudioListItem
+                    onPress={() => onItemPress(item, data)}
+                    audio={item}
+                    isPlaying={onGoingAudio?.id === item.id}
+                  />
+                );
+              }}
+            />
+          </>
+        )}
       </View>
     </AppModal>
   );
